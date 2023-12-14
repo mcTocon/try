bd6887acc39b1a601bb11c1c720a8c0756fadd99
mcTocon <142721987+mcTocon@users.noreply.github.com>
2023-12-14 23:27:02 +0200

    Update GameFactory.sol

--- GameFactory.sol
+++ GameFactory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.10;
+
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "./Helpers/FactoryHelper.sol";
